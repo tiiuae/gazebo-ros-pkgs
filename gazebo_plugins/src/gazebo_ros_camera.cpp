fea2bf6863a230d936d80e6e0a7ce9d5338e4056
Lassi Laiho <lassi.laiho@solita.fi>
2022-01-27 07:25:33 +0000

    Add video publishing support to ros camera

--- gazebo_plugins/src/gazebo_ros_camera.cpp
+++ gazebo_plugins/src/gazebo_ros_camera.cpp
@@ -26,11 +26,15 @@
 #include <gazebo_ros/conversions/builtin_interfaces.hpp>
 #include <gazebo_ros/node.hpp>
 #include <gazebo_ros/utils.hpp>
+#include <gst/app/gstappsink.h>
+#include <gst/app/gstappsrc.h>
 #include <image_transport/image_transport.hpp>
+#include <opencv2/opencv.hpp>
 #include <sensor_msgs/fill_image.hpp>
 #include <sensor_msgs/image_encodings.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/compressed_image.hpp>
 #include <sensor_msgs/point_cloud2_iterator.hpp>
 
 #include <algorithm>
@@ -44,6 +48,42 @@
 
 namespace gazebo_plugins
 {
+template <class Func>
+class defer {
+public:
+  defer(Func func) : func(std::move(func)), call(true) {}
+
+  defer(const defer&) = delete;
+  defer& operator=(const defer&) = delete;
+
+  defer(defer&& other)
+    : func(std::move(other.func)),
+      call(other.call) {
+    other.call = false;
+  }
+  defer& operator=(defer&& other) {
+    func = std::move(other);
+    call = other.call;
+    other.call = false;
+    return *this;
+  }
+
+  ~defer() {
+    if (call) func();
+  }
+
+private:
+ Func func;
+ bool call;
+};
+
+template <class T>
+void safeUnref(T*& ptr) {
+  if (ptr) {
+    gst_object_unref(ptr);
+  }
+}
+
 class GazeboRosCameraPrivate
 {
 public:
@@ -68,6 +108,18 @@ public:
 
   /// Image publishers.
   std::vector<image_transport::Publisher> image_pub_;
+
+  /// Video streaming.
+  std::vector<rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr> video_pub_;
+  std::vector<GMainLoop*> g_main_loop_;
+  std::vector<GstElement*> gst_source_;
+  std::mutex gst_mutex_;
+
+  bool convFbImgToI420 = true;
+  bool useCuda = false;
+  bool useCudaCustomParams = false;
+  bool useVaapi = false;
+  std::string ros_namespace_;
 
   /// Camera info publishers.
   std::vector<rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr> camera_info_pub_;
@@ -176,15 +228,17 @@ void GazeboRosCamera::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _
   }
 
   // Camera name
-  impl_->camera_name_ = _sdf->Get<std::string>("camera_name", _sensor->Name()).first;
+  impl_->camera_name_ = _sdf->Get<std::string>("cameraName", _sensor->Name()).first;
 
   // Get tf frame for output
   impl_->frame_name_ = gazebo_ros::SensorFrameID(*_sensor, *_sdf);
 
+  impl_->ros_namespace_ = _sdf->Get<std::string>("rosNamespace", "").first;
+  auto topic_prefix = impl_->ros_namespace_ + "/" + impl_->camera_name_;
   if (impl_->sensor_type_ != GazeboRosCameraPrivate::MULTICAMERA) {
     // Image publisher
     // TODO(louise) Migrate image_connect logic once SubscriberStatusCallback is ported to ROS2
-    const std::string camera_topic = impl_->camera_name_ + "/image_raw";
+    const std::string camera_topic = topic_prefix + "/image_raw";
     impl_->image_pub_.push_back(
       image_transport::create_publisher(
         impl_->ros_node_.get(), camera_topic, qos.get_publisher_qos(
@@ -195,9 +249,20 @@ void GazeboRosCamera::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _
     // RCLCPP_INFO(
     //   impl_->ros_node_->get_logger(), "Publishing images to [%s]", impl_->image_pub_.getTopic());
 
+    // Video publisher
+    const std::string video_topic = topic_prefix + "/color/video";
+    impl_->video_pub_.push_back(
+      impl_->ros_node_->create_publisher<sensor_msgs::msg::CompressedImage>(
+        video_topic, qos.get_publisher_qos(
+          video_topic, rclcpp::SensorDataQoS().reliable())));
+
+    RCLCPP_INFO(
+      impl_->ros_node_->get_logger(), "Publishing video to [%s]",
+      impl_->video_pub_.back()->get_topic_name());
+
     // Camera info publisher
     // TODO(louise) Migrate ImageConnect logic once SubscriberStatusCallback is ported to ROS2
-    const std::string camera_info_topic = impl_->camera_name_ + "/camera_info";
+    const std::string camera_info_topic = topic_prefix + "/camera_info";
     impl_->camera_info_pub_.push_back(
       impl_->ros_node_->create_publisher<sensor_msgs::msg::CameraInfo>(
         camera_info_topic, qos.get_publisher_qos(
@@ -209,8 +274,9 @@ void GazeboRosCamera::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _
 
   } else {
     for (uint64_t i = 0; i < impl_->num_cameras_; ++i) {
-      const std::string camera_topic =
-        impl_->camera_name_ + "/" + MultiCameraPlugin::camera_[i]->Name() + "/image_raw";
+      auto camera_name = MultiCameraPlugin::camera_[i]->Name();
+      auto topic_prefix = impl_->ros_namespace_ + "/" + camera_name;
+      auto camera_topic = topic_prefix + "/image_raw";
       // Image publisher
       impl_->image_pub_.push_back(
         image_transport::create_publisher(
@@ -219,11 +285,22 @@ void GazeboRosCamera::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _
 
       // RCLCPP_INFO(
       //   impl_->ros_node_->get_logger(), "Publishing %s camera images to [%s]",
-      //   MultiCameraPlugin::camera_[i]->Name().c_str(),
+      //   camera_name.c_str(),
       //   impl_->image_pub_.back().getTopic());
 
-      const std::string camera_info_topic =
-        impl_->camera_name_ + "/" + MultiCameraPlugin::camera_[i]->Name() + "/camera_info";
+      // Video publisher
+      auto video_topic = topic_prefix + "/video";
+      impl_->video_pub_.push_back(
+        impl_->ros_node_->create_publisher<sensor_msgs::msg::CompressedImage>(
+          video_topic, qos.get_publisher_qos(
+            video_topic, rclcpp::SensorDataQoS().reliable())));
+
+      RCLCPP_INFO(
+        impl_->ros_node_->get_logger(), "Publishing %s camera video to [%s]",
+        camera_name.c_str(),
+        impl_->video_pub_[i]->get_topic_name());
+
+      auto camera_info_topic = topic_prefix + "/camera_info";
       // Camera info publisher
       impl_->camera_info_pub_.push_back(
         impl_->ros_node_->create_publisher<sensor_msgs::msg::CameraInfo>(
@@ -232,13 +309,13 @@ void GazeboRosCamera::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _
 
       RCLCPP_INFO(
         impl_->ros_node_->get_logger(), "Publishing %s camera info to [%s]",
-        MultiCameraPlugin::camera_[i]->Name().c_str(),
+        camera_name.c_str(),
         impl_->camera_info_pub_[i]->get_topic_name());
     }
   }
 
   if (impl_->sensor_type_ == GazeboRosCameraPrivate::DEPTH) {
-    const std::string depth_topic = impl_->camera_name_ + "/depth/image_raw";
+    const std::string depth_topic = topic_prefix + "/depth/image_raw";
     // Depth image publisher
     impl_->depth_image_pub_ = image_transport::create_publisher(
       impl_->ros_node_.get(), depth_topic, qos.get_publisher_qos(
@@ -247,7 +324,7 @@ void GazeboRosCamera::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _
     // RCLCPP_INFO(impl_->ros_node_->get_logger(), "Publishing depth images to [%s]",
     //   impl_->depth_image_pub_.getTopic().c_str());
 
-    const std::string depth_info_topic = impl_->camera_name_ + "/depth/camera_info";
+    const std::string depth_info_topic = topic_prefix + "/depth/camera_info";
     // Depth info publisher
     impl_->depth_camera_info_pub_ =
       impl_->ros_node_->create_publisher<sensor_msgs::msg::CameraInfo>(
@@ -257,7 +334,7 @@ void GazeboRosCamera::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _
       impl_->ros_node_->get_logger(), "Publishing depth camera info to [%s]",
       impl_->depth_camera_info_pub_->get_topic_name());
 
-    const std::string point_cloud_topic = impl_->camera_name_ + "/points";
+    const std::string point_cloud_topic = topic_prefix + "/points";
     // Point cloud publisher
     impl_->point_cloud_pub_ = impl_->ros_node_->create_publisher<sensor_msgs::msg::PointCloud2>(
       point_cloud_topic, qos.get_publisher_qos(point_cloud_topic, rclcpp::QoS(1)));
@@ -269,7 +346,7 @@ void GazeboRosCamera::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _
 
   // Trigger
   if (_sdf->Get<bool>("triggered", false).first) {
-    const std::string trigger_topic = impl_->camera_name_ + "/image_trigger";
+    const std::string trigger_topic = topic_prefix + "/image_trigger";
     impl_->trigger_sub_ = impl_->ros_node_->create_subscription<std_msgs::msg::Empty>(
       trigger_topic, qos.get_subscription_qos(trigger_topic, rclcpp::QoS(1)),
       std::bind(&GazeboRosCamera::OnTrigger, this, std::placeholders::_1));
@@ -554,6 +631,224 @@ void GazeboRosCamera::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _
 
   param_change_callback_handler_ =
     impl_->ros_node_->add_on_set_parameters_callback(param_change_callback);
+
+  gst_init(nullptr, nullptr);
+  impl_->convFbImgToI420 = _sdf->Get<bool>("convFbImgToI420", true).first;
+  impl_->useCuda = _sdf->Get<bool>("useCuda", false).first;
+  impl_->useCudaCustomParams = _sdf->Get<bool>("useCudaCustomParams", false).first;
+  impl_->useVaapi = _sdf->Get<bool>("useVaapi", false).first;
+  for (uint64_t i = 0; i < impl_->num_cameras_; ++i) {
+    std::thread thread{
+      &GazeboRosCamera::startGstPipeline,
+      this
+    };
+    thread.detach();
+  }
+}
+
+void GazeboRosCamera::startGstPipeline() {
+  size_t camera_num;
+  {
+    std::lock_guard<std::mutex> gst_lock(impl_->gst_mutex_);
+    camera_num = impl_->g_main_loop_.size();
+    impl_->g_main_loop_.push_back(nullptr);
+    impl_->gst_source_.push_back(nullptr);
+  }
+  auto main_loop = g_main_loop_new(nullptr, FALSE);
+  if (!main_loop) {
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "Failed to create main loop"
+    );
+    return;
+  }
+  defer _0{[&] { g_main_loop_unref(main_loop); }};
+
+  GstElement* pipeline = gst_pipeline_new(nullptr);
+  if (!pipeline) {
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "Failed to create pipeline"
+    );
+    return;
+  }
+  defer _1{[&] { gst_object_unref(pipeline); }};
+
+  GstElement* source = gst_element_factory_make("appsrc", nullptr);
+  if (!source) {
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "Failed to create source"
+    );
+    return;
+  }
+  defer _2{[&] { gst_object_unref(source); }};
+  GString *appSrcFormat = g_string_new("I420");
+  if (!impl_->convFbImgToI420) {
+    appSrcFormat = g_string_assign(appSrcFormat, "RGB");
+  }
+
+  auto image_width = impl_->camera_[camera_num]->ImageWidth();
+  auto image_height = impl_->camera_[camera_num]->ImageHeight();
+  auto render_rate = impl_->camera_[camera_num]->RenderRate();
+  if (!isfinite(render_rate)) {
+    render_rate = 60.0;
+  }
+  g_object_set(G_OBJECT(source), "caps",
+      gst_caps_new_simple ("video/x-raw",
+        "format", G_TYPE_STRING, appSrcFormat->str,
+        "width", G_TYPE_INT, image_width,
+        "height", G_TYPE_INT, image_height,
+        "framerate", GST_TYPE_FRACTION, (unsigned int)render_rate, 1, nullptr),
+      "is-live", TRUE,
+      "do-timestamp", TRUE,
+      "stream-type", GST_APP_STREAM_TYPE_STREAM,
+      "format", GST_FORMAT_TIME, nullptr);
+  g_string_free(appSrcFormat, false);
+
+  GstElement* queue = gst_element_factory_make("queue", nullptr);
+  if (!queue) {
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "Failed to create queue"
+    );
+    return;
+  }
+  defer _3{[&] { gst_object_unref(queue); }};
+
+  GstElement* converter  = gst_element_factory_make("videoconvert", nullptr);
+  if (!converter) {
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "Failed to create converter"
+    );
+    return;
+  }
+  defer _4{[&] { gst_object_unref(converter); }};
+
+  GstElement* filterNV12;
+  defer _5{[&] { safeUnref(filterNV12); }};
+  GstElement* encoder = nullptr;
+  defer _6{[&] { safeUnref(encoder); }};
+
+  if (impl_->useCuda) {
+    encoder = gst_element_factory_make("nvh264enc", nullptr);
+    if (impl_->useCudaCustomParams) {
+      g_object_set(G_OBJECT(encoder), "bitrate", 2000, nullptr);
+      // rc-mode: 0 = default; 1 = constqp; 2 = cbr; 3 = vbr; 4 = vbr-minqp
+      g_object_set(G_OBJECT(encoder), "rc-mode", 2, nullptr);
+      g_object_set(G_OBJECT(encoder), "qos", true, nullptr);
+      g_object_set(G_OBJECT(encoder), "gop-size", 60, nullptr);
+    } else {
+      g_object_set(G_OBJECT(encoder), "bitrate", 800, nullptr);
+      g_object_set(G_OBJECT(encoder), "preset", 1, nullptr); //lower = faster, 6=medium
+    }
+    RCLCPP_INFO(
+      impl_->ros_node_->get_logger(),
+      "Use nvh264enc."
+    );
+  } else if (impl_->useVaapi) {
+    encoder = gst_element_factory_make("vaapih264enc", "AvcEncoder");
+    g_object_set(G_OBJECT(encoder), "bitrate", 2000, nullptr);
+    RCLCPP_INFO(
+      impl_->ros_node_->get_logger(),
+      "Use vaapih264enc"
+    );
+    filterNV12 = gst_element_factory_make("capsfilter", "FilterNV12");
+    if (!filterNV12) {
+      RCLCPP_ERROR(
+        impl_->ros_node_->get_logger(),
+        "Failed to create NV12 filter element"
+      );
+      return;
+    }
+    g_object_set(G_OBJECT(filterNV12), "caps",
+        gst_caps_new_simple ("video/x-raw",
+        "format", G_TYPE_STRING, "NV12",
+        "width", G_TYPE_INT, image_width,
+        "height", G_TYPE_INT, image_height,
+        "framerate", GST_TYPE_FRACTION, (unsigned int)render_rate, 1,
+        nullptr),
+        nullptr);
+  } else {
+    encoder = gst_element_factory_make("x264enc", nullptr);
+    g_object_set(G_OBJECT(encoder), "bitrate", 800, "speed-preset", 6, "tune", 4, "key-int-max", 10, nullptr);
+  }
+  if (!encoder) {
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "Failed to create encoder"
+    );
+    return;
+  }
+
+  GstElement* filterH264 = gst_element_factory_make("capsfilter", "FilterH264");
+  if (!filterH264) {
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "Failed to create H264 filter element"
+    );
+    return;
+  }
+  defer _7{[&] { gst_object_unref(filterH264); }};
+  g_object_set(G_OBJECT(filterH264), "caps",
+      gst_caps_new_simple ("video/x-h264",
+      "profile", G_TYPE_STRING, "main",
+      "stream-format", G_TYPE_STRING, "byte-stream",
+      nullptr),
+      nullptr);
+
+  GstElement* sink = gst_element_factory_make("appsink", nullptr);
+  if (!sink) {
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "H264 filter element failed."
+    );
+    return;
+  }
+  defer _8{[&] { gst_object_unref(sink); }};
+
+  // Connect all elements to pipeline
+  if (impl_->useVaapi) {
+    gst_bin_add_many(GST_BIN(pipeline), source, queue, converter, filterNV12, encoder, filterH264, sink, nullptr);
+  } else {
+    gst_bin_add_many(GST_BIN(pipeline), source, queue, converter, encoder, filterH264, sink, nullptr);
+  }
+
+  // Link all elements
+  gboolean link_ok;
+  if (impl_->useVaapi) {
+    link_ok = gst_element_link_many(source, queue, converter, filterNV12, encoder, filterH264, sink, nullptr);
+  } else {
+    link_ok = gst_element_link_many(source, queue, converter, encoder, filterH264, sink, nullptr);
+  }
+  if (!link_ok) {
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "Failed to link elements"
+    );
+    return;
+  }
+
+  impl_->g_main_loop_[camera_num] = main_loop;
+  g_main_loop_ref(main_loop);
+  impl_->gst_source_[camera_num] = source;
+  gst_object_ref(source);
+  defer _9{[&] {
+    std::lock_guard<std::mutex> gst_lock(impl_->gst_mutex_);
+    g_main_loop_unref(main_loop);
+    impl_->g_main_loop_[camera_num] = nullptr;
+    gst_object_unref(source);
+    impl_->gst_source_[camera_num] = nullptr;
+  }};
+
+  std::thread sink_thread{[&] {
+    while (publishVideoFrame(sink, camera_num)) {}
+  }};
+  gst_element_set_state(pipeline, GST_STATE_PLAYING);
+  g_main_loop_run(main_loop);
+  gst_element_set_state(pipeline, GST_STATE_NULL);
+  sink_thread.join();
 }
 
 void GazeboRosCamera::NewFrame(
@@ -573,11 +868,12 @@ void GazeboRosCamera::NewFrame(
   } else {
     sensor_update_time = MultiCameraPlugin::parent_sensor_->LastMeasurementTime();
   }
+  auto sensor_update_time_ros =
+    gazebo_ros::Convert<builtin_interfaces::msg::Time>(sensor_update_time);
 
   // Publish camera info
   auto camera_info_msg = impl_->camera_info_manager_[_camera_num]->getCameraInfo();
-  camera_info_msg.header.stamp = gazebo_ros::Convert<builtin_interfaces::msg::Time>(
-    sensor_update_time);
+  camera_info_msg.header.stamp = sensor_update_time_ros;
 
   impl_->camera_info_pub_[_camera_num]->publish(camera_info_msg);
 
@@ -585,8 +881,7 @@ void GazeboRosCamera::NewFrame(
 
   // Publish image
   impl_->image_msg_.header.frame_id = impl_->frame_name_;
-  impl_->image_msg_.header.stamp = gazebo_ros::Convert<builtin_interfaces::msg::Time>(
-    sensor_update_time);
+  impl_->image_msg_.header.stamp = sensor_update_time_ros;
 
   // Copy from src to image_msg
   sensor_msgs::fillImage(
@@ -595,6 +890,14 @@ void GazeboRosCamera::NewFrame(
 
   impl_->image_pub_[_camera_num].publish(impl_->image_msg_);
 
+  pushToGstPipeline(
+    _image,
+    _width,
+    _height,
+    sensor_update_time_ros,
+    _camera_num
+  );
+
   // Disable camera if it's a triggered camera
   if (nullptr != impl_->trigger_sub_) {
     SetCameraEnabled(false);
@@ -602,6 +905,107 @@ void GazeboRosCamera::NewFrame(
     std::lock_guard<std::mutex> lock(impl_->trigger_mutex_);
     impl_->triggered = std::max(impl_->triggered - 1, 0);
   }
+}
+
+void GazeboRosCamera::pushToGstPipeline(
+  const unsigned char * image,
+  unsigned int width,
+  unsigned int height,
+  const builtin_interfaces::msg::Time& time,
+  const int camera_num
+) {
+  guint size = width * height * 1.5;
+  if (!impl_->convFbImgToI420) {
+    size = width * height * 3;
+  }
+  GstBuffer* buffer = gst_buffer_new_allocate(nullptr, size, nullptr);
+
+  if (!buffer) {
+    RCLCPP_INFO(
+      impl_->ros_node_->get_logger(),
+      "gst_buffer_new_allocate failed"
+    );
+    return;
+  }
+
+  GstMapInfo map;
+
+  if (!gst_buffer_map(buffer, &map, GST_MAP_WRITE)) {
+    RCLCPP_INFO(
+      impl_->ros_node_->get_logger(),
+      "gst_buffer_map failed"
+    );
+    return;
+  }
+
+  if (impl_->convFbImgToI420) {
+    using namespace cv;
+	  // Color Conversion from RGB to YUV
+    Mat frame = Mat(height, width, CV_8UC3);
+    Mat frameYUV = Mat(height, width, CV_8UC3);
+    // frame.data = image;
+    frame.data = (uchar*)image;
+    cvtColor(frame, frameYUV, COLOR_RGB2YUV_I420);
+    memcpy(map.data, frameYUV.data, size);
+  } else {
+	  memcpy(map.data, image, size);
+	  // memcpy(map.data, (uchar *)image, size);
+  }
+  gst_buffer_unmap(buffer, &map);
+
+  std::lock_guard<std::mutex> gst_lock{impl_->gst_mutex_};
+  auto source = impl_->gst_source_[camera_num];
+  if (!source) {
+    RCLCPP_WARN(
+      impl_->ros_node_->get_logger(),
+      "Gstreamer source has been closed"
+    );
+    return;
+  }
+  GstFlowReturn ret = gst_app_src_push_buffer(
+    GST_APP_SRC(source),
+    buffer
+  );
+
+  if (ret != GST_FLOW_OK) {
+    /* something wrong, stop pushing */
+    RCLCPP_ERROR(
+      impl_->ros_node_->get_logger(),
+      "gst_app_src_push_buffer failed"
+    );
+    g_main_loop_quit(impl_->g_main_loop_[camera_num]);
+  }
+}
+
+bool GazeboRosCamera::publishVideoFrame(
+  GstElement* sink,
+  size_t camera_num
+) {
+  GstSample* sample = gst_app_sink_pull_sample(GST_APP_SINK(sink));
+  if (!sample) {
+    RCLCPP_INFO(
+      impl_->ros_node_->get_logger(),
+      "end of stream was reached"
+    );
+    return true;
+  }
+  defer _{[&] { gst_sample_unref(sample); }};
+  GstBuffer* buf = gst_sample_get_buffer(sample);
+  if (!buf) {
+    RCLCPP_WARN(
+      impl_->ros_node_->get_logger(),
+      "sample contains no data"
+    );
+    return true;
+  }
+  sensor_msgs::msg::CompressedImage frame;
+  frame.header.frame_id = buf->offset;
+  frame.header.stamp = impl_->ros_node_->get_clock()->now();
+  frame.format = "H264";
+  frame.data.resize(gst_buffer_get_size(buf), 0);
+  gst_buffer_extract(buf, 0, frame.data.data(), frame.data.size());
+  impl_->video_pub_[camera_num]->publish(frame);
+  return true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
